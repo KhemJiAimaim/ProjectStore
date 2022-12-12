7be8f4ebe60b56646ffe300895a67dbe708fa52b
pos <dragonaumaim@gmail.com>
2022-12-13 01:57:05 +0700

    add cart

--- store/models.py
+++ store/models.py
@@ -48,3 +48,34 @@ class Product(models.Model):
     def __str__(self) :
         return self.name
 
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=255, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True) #วัน-เวลาเพิ่มขุ้อมูลสินค้า
+
+    def __str__(self):
+        return self.cart_id
+    
+    class Meta:
+        db_table = 'cart'
+        ordering = ['date_added']
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product , on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart , on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'cartItem'
+        
+    #ผลรวมราคาขาย
+    def sub_total(self):
+        return self.product.price * self.quantity
+    
+    #ผลรวมต้นทุน
+    def sub_cost(self):   
+        return self.product.cost * self.quantity
+
+    def __str__(self):
+        return self.cart
+
