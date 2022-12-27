23c486a0d41383d37e59ef41093b8bb8c9f00370
pos <dragonaumaim@gmail.com>
2022-12-28 03:48:12 +0700

    C databases

--- DjangoPOS/settings.py
+++ DjangoPOS/settings.py
@@ -83,16 +83,24 @@ WSGI_APPLICATION = 'DjangoPOS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storedb',
-        'HOST': 'localhost',
-        'PASSWORD': '',
-        'USER': 'root',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.mysql',
+#       'HOST': 'localhost',
+ #       'PASSWORD': '',
+  #      'USER': 'root',
+   #     'PORT': '3306',
+    #}
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
